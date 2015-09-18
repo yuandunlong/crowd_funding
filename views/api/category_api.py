fb5afd767c9cfa105fccdbe923b7fe3637ba2de2
yuandunlong <yuandunlong@gmail.com>
2015-09-18 10:01:48 +0800

    push test

--- views/api/category_api.py
+++ views/api/category_api.py
@@ -3,12 +3,13 @@
 # @Author: yuandunlong
 # @Date:   2015-09-11 16:38:41
 # @Last Modified by:   yuandunlong
-# @Last Modified time: 2015-09-18 09:56:50
+# @Last Modified time: 2015-09-18 10:01:35
 
 from flask import request,Blueprint
 from utils.decorator import json_response
 from services import category_service
 category_api=Blueprint('category_api',__name__)
+
 @category_api.route('/public/category/get_all_categories',methods=['GET'])
 @json_response
 def get_all_categories(result):
