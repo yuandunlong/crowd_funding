44e1ab3ca7dee4c95982cebfc1928a9fbef6ba58
yuandunlong <yuandunlong@gmail.com>
2015-09-18 10:11:00 +0800

    add folders

---
+++ views/admin/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: yuandunlong
+# @Date:   2015-09-18 10:10:39
+# @Last Modified by:   yuandunlong
+# @Last Modified time: 2015-09-18 10:10:39
