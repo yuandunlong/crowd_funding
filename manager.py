b1d958e6578fd6d1238d77818e5e18d8a182e290
yuandunlong <ydl@192.168.0.103>
2015-12-06 12:16:39 +0800

    order

--- manager.py
+++ manager.py
@@ -5,12 +5,12 @@
 # @Last Modified by:   yuandunlong
 # @Last Modified time: 2015-11-18 17:50:38
 # -*- coding: utf-8 -*-
-from flask.ext.script import Shell, Manager
+from flask_script import Shell, Manager
 
 from flask_web import app,create_app
 from database import models
 from database.models import db
-from flask.ext.migrate import Migrate, MigrateCommand
+from flask_migrate import Migrate, MigrateCommand
 from flask import current_app
 def _make_context():
     return dict(app=app, db=db, models=models,current_app=current_app)
